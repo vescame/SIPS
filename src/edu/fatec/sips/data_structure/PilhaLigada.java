1a3d1eac6090f4dd7a6bfa0b5c8c0c54abbaf4a6
'vescame' <'v.escame@gmail.com'>
2020-09-19 10:23:13 -0300

    Add pilha dinamica simples

---
+++ src/edu/fatec/sips/data_structure/PilhaLigada.java
@@ -0,0 +1,56 @@
+package edu.fatec.sips.data_structure;
+
+import edu.fatec.sips.data_structure.node.Node;
+
+public class PilhaLigada<T> {
+	private Node<T> top;
+	private int size;
+	
+	public PilhaLigada() {
+		top = null;
+		size = 0;
+	}
+	
+	public boolean isEmpty() {
+		return top == null;
+	}
+	
+	public final int getSize() {
+		return size;
+	}
+
+	public final T peek() {
+		T element = null;
+		if (isEmpty()) {
+			System.out.println("EMPTY LIST");
+		} else {
+			element = top.getElement();
+		}
+		
+		return element;
+	}
+	
+	public void push(final T element) {
+		final Node<T> node = new Node<T>(element);
+		if (isEmpty()) {
+			top = node;
+		} else {
+			node.setNext(top);
+			top = node;
+		}
+		size++;
+	}
+	
+	public final T pop() {
+		T element = null;
+		if (isEmpty()) {
+			System.out.println("EMPTY STACK");
+		} else {
+			element = top.getElement();
+			top = top.getNext();
+			size--;
+		}
+		
+		return element;
+	}
+}
