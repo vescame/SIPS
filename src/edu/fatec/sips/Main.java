ed87fae5b673f42c60c932f7ac327845c5a2e5de
Wiliam Nascimento <wilnasc990s3@gmail.com>
2020-10-13 18:53:54 -0300

    Menu cronograma de atividade concluído.

--- src/edu/fatec/sips/Main.java
+++ src/edu/fatec/sips/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import edu.fatec.sips.view.MenuPrincipal;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception, HeadlessException, IOException {
 		MenuPrincipal menuPrincipal = new MenuPrincipal();
 		menuPrincipal.menuPrincipal(0);
